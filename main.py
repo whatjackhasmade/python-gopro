7be0a3d5f6888520c7c008a6bf6890b9ae37e7d8
jackpritchard <jack.pritchard@sonovate.com>
2023-07-27 10:18:21 +0100

    feat: compile

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
+import glob
+import os
+import time
+import subprocess
 from open_gopro import WiredGoPro
 from rich.console import Console
+
+def combine_mp4_files(input_files, output_file):
+    # Convert filenames to lowercase with .mp4 extension
+    input_files = [file.lower() for file in input_files]
+
+    # Prepare the FFMPEG command with re-encoding
+    ffmpeg_cmd = ["ffmpeg"]
+    for file in input_files:
+        ffmpeg_cmd.extend(["-i", file])
+    ffmpeg_cmd.extend(["-filter_complex", f"concat=n={len(input_files)}:v=1:a=1", "-c:v", "libx264", "-c:a", "aac", output_file])
+
+    # Run the FFMPEG command using subprocess
+    try:
+        subprocess.run(ffmpeg_cmd, check=True)
+        print(f"MP4 files successfully combined into {output_file}.")
+    except subprocess.CalledProcessError as e:
+        print(f"Error occurred while combining MP4 files: {e}")
 
 # rich consoler printer
 console = Console()
@@ -10,6 +31,12 @@ last_4_characters_of_serial_number = serial_number[-4:]
 # Connect to the camera
 gopro = WiredGoPro(last_4_characters_of_serial_number)
 gopro.open()
+gopro.http_command.set_turbo_mode(mode=True)
+
+# Empty out the output directory of any .mp4 files
+files = glob.glob('./output/*.MP4')
+for f in files:
+	os.remove(f)
 
 # Download all of the files from the camera
 media_list = [x["n"] for x in gopro.http_command.get_media_list().flatten]
@@ -18,6 +45,18 @@ for file in media_list:
 
 			# Get the media info
 			info = gopro.http_command.get_media_info(file=file)
-			console.print(f"Downloading the media {file}")
+			cre = info["cre"]
+
+			console.print(f"••• Downloading the media {file}")
 			gopro.http_command.download_file(camera_file=file, local_file=output)
-			console.print(f"Success!! :smiley: File has been downloaded to {output}")
+			console.print(f"✓ File has been downloaded to {output}")
+
+# Wait for 5 seconds
+console.print("Waiting for 5 seconds...")
+time.sleep(5)
+
+files = glob.glob('./output/*.MP4')
+# get files as an array
+files.sort()
+
+combine_mp4_files(files, f"./output/Final.MP4")
